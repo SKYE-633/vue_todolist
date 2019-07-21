b03422e933a2e8f1fe37b2164d242a731ab36367
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-21 10:31:12 +0800

    input, button OK, no template

--- src/TodoList.vue
+++ src/TodoList.vue
@@ -3,18 +3,30 @@
   <div>
     <div>Jquery To Do List</div>
     <div>Simple Todo List with adding and filter by diff status</div>
-    <input />
-    <button>Add</button>
+    <input v-model="inputValue"/>
+    <button @click="handleSubmit">Add</button>
   </div>
   <ul>
-    <li></li>
+    
   </ul>
 </div>
-
 </template>
 
 <script>
-
+export default {
+  data () {
+    return {
+      inputValue:'',
+      list:[]
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue=''
+    }
+  }
+}
 </script>
 
 <style>
