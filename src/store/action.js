77b8cdeb4302b35bdce53c6a5c19be093d65d2db
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-23 22:36:01 +0800

    Update action.js

--- src/store/action.js
+++ src/store/action.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://5d36b54b86300e0014b6452c.mockapi.io/api/v1/lists';
+const baseUrl = '';
 export const actions = {
     getList({ commit }) {
         axios.get(baseUrl).then(response => commit('reloadItems', response.data));
