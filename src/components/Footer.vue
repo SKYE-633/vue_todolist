06f851f40bada4290d38265610ec435daa9e7e5d
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-23 21:09:05 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,19 @@
 <template>
     <div>
-      <div id="filters">
+      <ul id="filters">
         <li><a href="#" @click="display('All')">ALL</a></li>
         <li><a href="#" @click="display('Actived')">Active</a></li>
         <li><a href="#" @click="display('Completed')">Complete</a></li>
-    </div>
+    </ul>
     </div>
 </template>
 <script>
 export default {
     name: "Filters",
     methods: {
-    display(flag) {
-      if (flag === "All") {
-        this.$store.getters.getAllItems;
-      } else if (flag === "Actived") {
-        this.$store.getters.getActiveItems;
-      } else {
-        this.$store.getters.getCompletedItems;
-      }
-    }
+        display(flag) {
+         this.$store.commit("updateDisplayItems", flag);
+        }
   }
 }
 </script>
