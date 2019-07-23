9fbe6dafe02dc300323403081dcff6696bfe8a0c
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-23 22:44:20 +0800

    add router

---
+++ src/components/Mine.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img src="../../face.jpg" style="width:200px;height:200px;" alt="">
+    <p>姓名：{{getLoginUser}}</p>
+    <p>电话号码:{{getUserPhone}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  computed: {
+    getLoginUser: function() {
+      return this.$store.state.name;
+    },
+    getUserPhone: function() {
+      return this.$store.state.phone;
+    }
+  }
+};
+</script>
