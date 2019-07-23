fbf0758b0a04aa2c56f5e3b17148dfa9ff685fc7
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-23 20:53:47 +0800

    Update action.js

--- src/store/action.js
+++ src/store/action.js
@@ -1,3 +1,18 @@
-const store = new Vuex.Store({
-    
-})
+import axios from 'axios'
+
+const baseUrl = 'http://5d36b54b86300e0014b6452c.mockapi.io/api/v1/lists';
+export const actions = {
+    getList({ commit }) {
+        axios.get(baseUrl).then(response => commit('reloadItems', response.data));
+    },
+    addItem(context, data) {
+        axios.post(baseUrl, data).then(() => this.dispatch('getList'));
+    },
+    updateItem(context, data) {
+        axios.put(`${baseUrl}/${data.id}`, data).then(() => this.dispatch('getList'));
+    },
+    deleteItem(context, data) {
+        axios.delete(`${baseUrl}/${data.id}`, data).then(() => this.dispatch('getList'));
+    },
+
+}
