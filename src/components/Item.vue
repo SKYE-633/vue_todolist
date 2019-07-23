d6543dd26ab8bbd4d384fdbbe72731d2fb7df0ad
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-23 21:07:41 +0800

    Update Item.vue

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <input name="todo-list" type="checkbox" 
-        v-model="item.isCompleted" @change="update" />
-        <span>{{item.content}}</span>
+        <div v-if="!updateItemFlag">
+            <input name="todo-list" type="checkbox" 
+            v-model="item.isCompleted" @change="update" />
+            <button @click="deleteItem"></button>
+        </div>
+        <div v-else>
+            <input type="text" @change="updateItem()" v-model="content"/>
+        </div>
     </div>
 </template>
 <script>
@@ -11,11 +16,36 @@ export default {
     props: {
     item: Object
   },
+  data () {
+  return {
+    updateItemFlag: false,
+    content: ""
+    };
+  },
     methods: {
+        resetFlag() {
+          this.updateItemFlag = !this.updateItemFlag;
+        },
+        updateCompleted() {
+          this.update();
+        },
+        updateItem() {
+          this.update();
+          this.resetFlag();
+        },
         update() {
-      this.$store.commit("update", this.item.id);
-    }
-  }
+          this.$store.dispatch("updateItem", {
+            id: this.item.id,
+            content: this.content == "" ? this.item.content : this.content,
+            isCompleted: this.item.isCompleted
+          });
+        },
+        deleteItem(){
+          this.$store.dispatch("deleteItem", {
+            id: this.item.id
+          });
+        }
+      }
 }
 </script>
 <style>
